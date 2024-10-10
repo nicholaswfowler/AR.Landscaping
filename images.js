bc6edfbfe894bac54048f6ead2eddf2d25011910
nicholaswfowler <153998644+nicholaswfowler@users.noreply.github.com>
2024-10-09 20:29:29 -0500

    Updated instructions to add and remove images

--- images.js
+++ images.js
@@ -1,12 +1,16 @@
 // TO ADD IMAGES
-// 1) At the end of line 11 add the name of the new image file with surrounding quotes ('') and a comma
+// 1) At the end of Line 15 add the name of the new image file with surrounding quotes ('') and a comma
 // 2) You should add something that looks similiar to (, 'examplefilename.jpg') between the arrows (--> <--) in the example below
 // ex) const images = ['landscaping1.jpg', 'landscaping2.jpg', 'landscaping3.jpg', 'landscaping4.jpg', 'landscaping5.jpg', 'landscaping6.jpg'-->, 'examplefilename.jpg'<--];
+// 3. Click "Commit changes" in the top right area
+// 4. Click "Commit changes" in the pop-up that will show up
 
 // TO REMOVE IMAGES
 // 1) Highlight and delete from 1 comma to the next
 // 2) You should delete between the arrows (--> <--) in the example below
 // ex) const images = ['landscaping1.jpg', 'landscaping2.jpg', 'landscaping3.jpg', 'landscaping4.jpg', 'landscaping5.jpg' -->, 'landscaping6.jpg'<--];
+// 3. Click "Commit changes" in the top right area
+// 4. Click "Commit changes" in the pop-up that will show up
 
 const images = ['landscaping1.jpg', 'landscaping2.jpg', 'landscaping3.jpg', 'landscaping4.jpg', 'landscaping5.jpg', 'landscaping6.jpg'];
 
